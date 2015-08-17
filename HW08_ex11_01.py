2d32946b5d141aad5a6ece2dd6bd57a3b9982598
Max Curran <mtcurran@berkeley.ischool.edu>
2015-08-17 00:07:47 -0700

    Max Curran HW08 11_01 and 11_02 submission

--- HW08_ex11_01.py
+++ HW08_ex11_01.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python
 # Exercise 1  
 # Write a function that reads the words in words.txt and stores them as keys
-# in a dictionary (returning the dictionary). It doesn’t matter what the 
+# in a dictionary (returning the dictionary). It doesnt matter what the 
 # values are. Then you can use the in operator as a fast way to check whether
 # a string is in the dictionary.
 ##############################################################################
 
 def store_to_dict():
-    pass
+	with open("words.txt", "r") as f:
+		words_list = f.readlines()
+
+	words_dict = {}
+	i = 0
+	for line in words_list:
+		words_dict[line.strip()] = i
+		i += 1
+
+	return words_dict
 
 
 ##############################################################################
--- HW08_ex11_02.py
+++ HW08_ex11_02.py
@@ -20,10 +20,9 @@
 ##############################################################################
 
 # Imports
+import matplotlib.pyplot as plt
 
 # Body
-
-pledge_histogram = {}
 
 def histogram_old(s):
     d = dict()
@@ -35,19 +34,51 @@ def histogram_old(s):
     return d
 
 def histogram_new(s):
-    pass
+    d = dict()
+    for c in s:
+        #Will be 1 for words found once, add one to values for words found more than once
+        d[c] = 1 + d.get(c, 0)
+    return d
+
 
 def get_pledge_list():
     """ Opens pledge.txt and converts to a list, each item is a word in 
     the order it appears in the original file. returns the list.
     """
-    # Your code here.
-    pass
-    #return pledge_list (uncomment this)
+
+    #Reads lines from text file into list
+    with open("pledge.txt", "r") as f:
+        pledge_list0 = f.readlines()
+
+    pledge_list1 = []
+
+    #Strips whitespace and carriage returns from lines
+    for line in pledge_list0:
+        pledge_list1.append(line.strip())
+
+    #Converts to string and joins lines by spaces
+    pledge_list2 = ' '.join(pledge_list1)
+
+    #Splits string into list by spaces
+    pledge_list3 = pledge_list2.split()
+
+    #Strips commas from words in list
+    pledge_list4 = [word.strip(",") for word in pledge_list3]
+
+    return pledge_list4
 
 ##############################################################################
 def main():  # DO NOT CHANGE BELOW
     print histogram_new(get_pledge_list())
+    pledge_histogram = histogram_new(get_pledge_list())
+
+    #Plotting histogram
+    plt.bar(range(len(pledge_histogram)), pledge_histogram.values(), align = "center")
+    plt.xticks(range(len(pledge_histogram)), list(pledge_histogram.keys()))
+    locs, labels = plt.xticks()
+    plt.setp(labels, rotation=90)
+
+    plt.show()
 
 if __name__ == '__main__':
     main()
